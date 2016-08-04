b97cbb37cf8512e4e01aefff1a7ec2653a5e0ad2
ThereIsNoWindBag <mgu4@naver.com>
2016-08-05 00:37:07 +0900

    Init project

---
+++ TCG/Assets/02.Scripts/playArcher.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playArcher : MonoBehaviour
+{
+    int unitNum = 1; //유닛 번호
+
+    void Awake ()
+    {
+        int attack  = unitsInfo.attack[unitNum];
+        int hp      = unitsInfo.hp[unitNum];
+        int speed   = unitsInfo.speed[unitNum];
+        int control = unitsInfo.control[unitNum];
+        int range   = unitsInfo.range[unitNum];
+    }
+	void Start ()
+    {
+        
+	}
+	
+
+	void Update ()
+    {
+	
+	}
+}
