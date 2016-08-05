4eef7aa3a06a69296dfd0ca9f163a3b9db93f208
kimjh7204 <kimjh7204@gmail.com>
2016-08-06 05:01:19 +0900

    업뎃
    유닛 UI구현
    유닛 컨트롤 권한 주기
    버그 다소있음

---
+++ TCG(updated)/Assets/02.scripts/playArcher.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playArcher : MonoBehaviour
+{
+    public static int unitNum = 0;              //유닛 번호
+
+    public static int attack;
+    public static int hp;
+    public static int speed;
+    public static int control;
+    public static int range;
+
+    public static bool ctrlSelected = false;    //컨트롤 권한 여부
+
+    private GameObject arrowObj;                //Arrow 캔버스를 오브젝트로 담을 변수
+    private GameObject unitUIObj;               //unitUI 캔버스를 오브젝트로 담을 변수
+
+    public static bool unitUIOn = false;        //unitUI 표시 여부
+
+    private Animator UIpop;
+
+
+    void Awake ()
+    {
+        attack  = unitsInfo.attack[unitNum];
+        hp      = unitsInfo.hp[unitNum];
+        speed   = unitsInfo.speed[unitNum];
+        control = unitsInfo.control[unitNum];
+        range   = unitsInfo.range[unitNum];
+
+        arrowObj = GetComponent<Transform>().FindChild("Arrow").gameObject;   //오브젝트 하위의 Arrow캔버스를 찾는다
+        unitUIObj = GetComponent<Transform>().FindChild("/Archer Girl/unitUI/UIPanel").gameObject; //오브젝트 하위의 UIPanel을 찾는다
+
+        UIpop = GetComponent<Transform>().FindChild("/Archer Girl/unitUI/UIPanel").gameObject.GetComponent<Animator>(); //하위 항목의 UIPanel에서 Animator컴포넌트를 찾아온다
+
+    }
+
+	void Start ()
+    {
+        
+	}
+	
+
+	void Update ()
+    {
+
+        if(ctrlSelected == true)                           //컨트롤 권한을 획득
+        {
+            arrowObj.SetActive(true);                      //화살표 나타나게하는거
+
+            if (unitUIOn == true && !Player1.selectCtrl)
+            {
+                unitUIObj.SetActive(true);
+                UIpop.SetBool("click", true);
+            }
+        }
+        else if(ctrlSelected == false)                    //컨트롤 권한을 해제
+        {
+            arrowObj.SetActive(false);                    //화살표 사라지게함
+        }
+
+        
+        if (UIMgr.unitUIBtn == 5)
+        {
+            UIpop.SetBool("click", false);
+            Debug.Log(UIpop.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.NonSelected"));
+            if(UIpop.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.NonSelected"))
+            {
+                UIMgr.unitUIBtn = 0;
+                unitUIObj.SetActive(false);
+                unitUIOn = false;
+                Debug.Log("1");
+            }
+        }
+
+	
+	}
+
+}
---
+++ TCG/Assets/02.Scripts/Game.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game : MonoBehaviour {
+
+    public enum Status { P1, P2, Pause, P1won, P2won };
+    public static Game instance;
+
+    private Status status;
+
+    public static Game getInstance() {
+        return instance;
+    }
+
+    // Use this for initialization
+    void Start () {
+        game = this;
+        status = Status.P1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //P1일때 Player1 활성화.
+        //P2일때 Player2 활성화.
+        //Pause일때 게임 멈추기.
+	
+	}
+}
